50d931c32b7587f1aa0893dca197d75daa1361b7
Emre Buyuran <emre.byrn@gmail.com>
2021-03-30 14:18:45 +0300

    Refactoring

--- Business/Abstract/IUserService.cs
+++ Business/Abstract/IUserService.cs
@@ -10,6 +10,10 @@ namespace Business.Abstract
 {
     public interface IUserService
     {
+        IResult Update(User user);
+        IResult Delete(User user);
+        IDataResult<List<User>> GetAll();
+        IDataResult<User> GetById(int userId);
         IDataResult<List<OperationClaim>> GetClaims(User user);
         IResult Add(User user);
         IDataResult<User> GetByMail(string email);
